2b5ec045a83eafcfe54cc21ef1770434d619ba1f
Anwar Abdi <Anwarabdi2090@gmail.com>
2024-12-07 16:43:22 +0300

    set up the model

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Classes extends Model
+class ClassModel extends Model
 {
-    //
+    use HasFactory;
+
+    // Specify the fields that can be mass-assigned
+    protected $fillable = ['class_name', 'class_fee', 'status'];
 }
